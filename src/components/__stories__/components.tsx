4857be4b3bab13f992089023b8c5996a9d8009f6
Scott Cinatl <scottcinatl@gmail.com>
2019-04-23 13:17:20 -0700

    Updated default Button component and added an AnimatedButton component

--- src/components/__stories__/components.tsx
+++ src/components/__stories__/components.tsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import { action } from '@storybook/addon-actions'
-import { color, withKnobs } from '@storybook/addon-knobs'
-import { storiesOf } from '@storybook/react'
-import { Button } from '../Button'
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { boolean, withKnobs, select, text } from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
+import { AnimatedButton } from "../AnimatedButton";
+import { Button } from "../Button";
 
-storiesOf('Components', module)
+storiesOf("Components", module)
   .addDecorator(withKnobs)
-  .add('Button', () => {
-    const buttonColor = color('Color', '#2196F3')
-    return <Button onSubmit={action('onSubmit')} color={buttonColor} />
+  .add("AnimatedButton", () => {
+    const type = select(
+      "Button Type",
+      ["basic", "primary", "secondary", "none"],
+      "primary"
+    );
+    return (
+      <AnimatedButton
+        hiddenName={text("Hidden Text", 'Are You Sure?')}
+        isLoading={boolean("isLoading", false)}
+        onSubmit={action("onSubmit")}
+        type={type}
+        visibleName={text("Visible Text", 'Accept')}
+      />
+    );
   })
+  .add("Button", () => {
+    const type = select(
+      "Button Type",
+      ["basic", "primary", "secondary", "none"],
+      "primary"
+    );
+    return (
+      <Button
+        isLoading={boolean("isLoading", false)}
+        name="Button"
+        onSubmit={action("onSubmit")}
+        type={type}
+      />
+    );
+  });
